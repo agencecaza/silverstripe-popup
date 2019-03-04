9c6d8510a3f71afd15ee44622fb6bdcba7ac378a
Stéphane Guevremont <stephane.guevremont@gmail.com>
2019-03-04 13:28:57 -0500

    Update PopupLeftAndMain.php

--- src/PopupLeftAndMain.php
+++ src/PopupLeftAndMain.php
@@ -56,6 +56,10 @@ class PopupLeftAndMain extends LeftAndMain {
 				 '',
 					CheckboxField::create('Online', _t('PopupLeftAndMain.ONLINE','Put online')),
 					CheckboxField::create('Reset', _t('PopupLeftAndMain.RESETDATETIME','Reset the cookie datetime')),
+					TextField::create(
+						'DisplayDelay',
+						_t('PopupLeftAndMain.DISPLAYDELAY','Display delay')
+					)->setDescription( _t('PopupLeftAndMain.INSECONDS','in seconds') ),
 					$date = DatetimeField::create('DateTime', _t('PopupLeftAndMain.STARTDATETIME','Start datetime'))->setDatetimeFormat('YYYY-MM-dd HH:mm:ss')->setHTML5(false),
 					$dateend = DatetimeField::create('DateTimeEnd', _t('PopupLeftAndMain.ENDDATETIME','End datetime'))->setDatetimeFormat('YYYY-MM-dd HH:mm:ss')->setHTML5(false),
 					DropdownField::create(
