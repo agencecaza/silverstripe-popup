93eb620202f84e7a96c053d41c1974b8ad603339
agencecaza <51217595+agencecaza@users.noreply.github.com>
2019-07-04 08:48:27 -0400

    Update DateTime.js

--- client/javascript/DateTime.js
+++ client/javascript/DateTime.js
@@ -3,12 +3,9 @@ jQuery.noConflict();
 (function($) {
 
 	$('input.datetime').datetimepicker({
-		timepicker:false,
-		mask:true,
 		format:'Y-m-d H:i:s',
 		lang:'fr',
 		timepicker:true,
-
 	});
 
 }(jQuery));
